852775ffa971ba088dd2d4faab949ee78b948ba6
rolfgm <rolfglenny@gmail.com>
2021-04-26 08:49:44 +0000

    summary update

--- R/06_model_ii.R
+++ R/06_model_ii.R
@@ -13,7 +13,7 @@ Data_aug <- read_tsv(file = "data/03_data_aug.tsv.gz")
 
 # Plot data ---------------------------------------------------------------
 Corr_plot <- ggpairs(Data_aug,
-                     mapping = aes(colou = Country_Name),
+                     mapping = aes(colour = Country_Name),
                      columns = c('pH', 'Temp', 'TS', 'VS', 'VFA', 'CODt', 'CODs', 
                                  'perCODsbyt', 'NH4', 'Prot', 'Carbo', 'Latrine_Depth_cm'),
                      columnLabels = c('pH', 'Temp', 'TS', 'VS', 'VFA', 'CODt', 'CODs', 
