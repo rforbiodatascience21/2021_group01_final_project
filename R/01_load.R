5867aaf84375af7e8779d83074106c060c75c626
TennaP <tennaped@gmail.com>
2021-04-27 13:12:38 +0000

    Commit original load

--- R/01_load.R
+++ R/01_load.R
@@ -13,23 +13,6 @@ if (!file.exists("processed.cleveland.data")) {
 require(tools)
 md5sum("processed.cleveland.data")
 
-
-# Load data ---------------------------------------------------------------
-<<<<<<< HEAD
-Heart_cleve_raw <- read_file(file = "data/_raw/cleveland.data")
-
-# Wrangle data ------------------------------------------------------------
-# change placment of \n
-
-Data <- Heart_cleve_raw %>% 
-  str_split("\n") 
-
-class(Data)
-
-# Write data --------------------------------------------------------------
-
-write_tsv(x = Data,file = "data/01_Data_temp.tsv.gz",delim =" ")
-=======
 Cleveland_raw <- read.csv("processed.cleveland.data", header = FALSE)
 
 # Wrangle data ------------------------------------------------------------
