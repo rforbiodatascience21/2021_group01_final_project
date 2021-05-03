0e1c6366a19c6eb4ed4396049a77d3e3d668e957
rolfgm <rolfglenny@gmail.com>
2021-05-03 07:10:03 +0000

    Making categorial columns

--- R/03_augment.R
+++ R/03_augment.R
@@ -13,7 +13,11 @@ Data <- read_tsv(file = "data/02_data.tsv.gz")
 # Wrangle data ------------------------------------------------------------
 Data_aug <- Data %>%
   mutate(Location_num = case_when(Location == "Cleveland" ~ 0, Location == "Switzerland" ~ 1, Location == "Hungarian" ~ 2, Location == "Long Beach" ~ 3),
-         Diagnosis_of_disease = case_when(diagnosis_of_heart_disease == 0 ~ "Not present", diagnosis_of_heart_disease >= 1 ~ "Present"))
+         Diagnosis_of_disease = case_when(diagnosis_of_heart_disease == 0 ~ "Not present", diagnosis_of_heart_disease >= 1 ~ "Present"),
+         Sex_cat = case_when(Sex == 0 ~ "Female", Sex == 1 ~ "Male"),
+         Fasting_blood_sugar_cat = case_when(Fasting_blood_sugar == 0 ~ "Low", Fasting_blood_sugar == 1 ~ "High"),
+         Resting_electrocardiographic_cat = case_when(Resting_electrocardiographic == 0 ~ "Normal", Resting_electrocardiographic == 1 ~ "ST-T abnormality", Resting_electrocardiographic == 2 ~ "LV hypertrophy"),
+         Exercise_induced_angina_cat = case_when(Exercise_induced_angina == 1 ~ "Present", Exercise_induced_angina == 0 ~ "Not present"))
 
 # Write data --------------------------------------------------------------
 write_tsv(x = Data_aug,
--- R/06_model_ii.R
+++ R/06_model_ii.R
@@ -13,14 +13,14 @@ Data_aug <- read_tsv(file = "data/03_data_aug.tsv.gz")
 
 # Plot data ---------------------------------------------------------------
 Corr_plot <- ggpairs(Data_aug,
-                     mapping(colour = diagnosis_of_heart_disease),
+                     mapping = aes(colour = Diagnosis_of_disease),
                      columns = c("Age","Sex", "Chest_pain_type",
                                  "Resting_blood_pressure","Serum_cholestoral",
                                  "Fasting_blood_sugar","Resting_electrocardiographic",
                                  "Maximum_heart_rate_achieved","Exercise_induced_angina",
                                  "ST_depression_induced_by_exercise",
                                  "The_slope_of_the_peak_exercise_ST_segment",
-                                 "diagnosis_of_heart_disease")) +
+                                 "Diagnosis_of_disease")) +
   theme_minimal() +
   theme(legend.position = "bottom") +
   labs(title = "Correlation plots of of environmental factors by country")
