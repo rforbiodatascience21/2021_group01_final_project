bddb5ebf5e6102df6fecacf912913d85bb2bf707
TennaP <tennaped@gmail.com>
2021-05-03 06:45:16 +0000

    Commit new mutate in aug

--- R/03_augment.R
+++ R/03_augment.R
@@ -12,7 +12,8 @@ Data <- read_tsv(file = "data/02_data.tsv.gz")
 
 # Wrangle data ------------------------------------------------------------
 Data_aug <- Data %>%
-  mutate(Location_num = case_when(Location == "Cleveland" ~ 0, Location == "Switzerland" ~ 1, Location == "Hungarian" ~ 2, Location == "Long Beach" ~ 3))
+  mutate(Location_num = case_when(Location == "Cleveland" ~ 0, Location == "Switzerland" ~ 1, Location == "Hungarian" ~ 2, Location == "Long Beach" ~ 3),
+         Diagnosis_of_disease = case_when(diagnosis_of_heart_disease == 0 ~ "Not present", diagnosis_of_heart_disease >= 1 ~ "Present"))
 
 # Write data --------------------------------------------------------------
 write_tsv(x = Data_aug,
