fb54c343ede58519c91ca06b7ad5496fea767847
rolfgm <rolfglenny@gmail.com>
2021-05-10 17:09:59 +0000

    06 cleanup

--- R/06_cholestoral_analysis.R
+++ R/06_cholestoral_analysis.R
@@ -5,7 +5,7 @@ rm(list = ls())
 library(tidyverse)
 library(ggplot2)
 library(ggpubr)
-library(broom)  # devtools::install_github("tidymodels/broom")
+library(broom)
 library(cowplot)
 library(viridis)
 
@@ -17,7 +17,11 @@ Data_aug <- read_tsv(file = "data/03_data_aug.tsv.gz")
 
 # Plot data ---------------------------------------------------------------
 
-Data <- Data_selection(data = Data_aug,var = c("Location","Serum_cholestoral","Diagnosis_of_disease"), rm_na = TRUE, rm_na_from = c("Serum_cholestoral")) 
+Data <- Data_selection(data = Data_aug,var = c("Location",
+                                               "Serum_cholestoral",
+                                               "Diagnosis_of_disease"), 
+                       rm_na = TRUE, 
+                       rm_na_from = c("Serum_cholestoral")) 
 
 Location_cholesterol_plot <- Data %>%
   ggplot(mapping = aes(x = Serum_cholestoral,
@@ -25,8 +29,8 @@ Location_cholesterol_plot <- Data %>%
   geom_boxplot(aes(fill = Location),alpha = 0.5) +
   theme_classic() +
   theme(legend.position = "none" ) +
-  scale_color_viridis(discrete=TRUE) +
-  scale_fill_viridis(discrete=TRUE)
+  scale_color_viridis(discrete = TRUE) +
+  scale_fill_viridis(discrete = TRUE)
 
 mean_cholesterol <- Data %>% 
   group_by(Location) %>% 
@@ -43,18 +47,18 @@ mean_string <- format_tsv(
 Location_disease_plot <- Data %>%
   ggplot(aes(y = Location,
              fill = Diagnosis_of_disease)) +
-  geom_bar(position = position_dodge(),alpha = 0.5) +
+  geom_bar(position = position_dodge(), alpha = 0.5) +
   theme_classic() +
   theme(legend.position = "none") +
   xlab("Purple = people with disease not present
        Yellow = people with disease present") +
-  scale_color_viridis(discrete=TRUE) +
-  scale_fill_viridis(discrete=TRUE)
+  scale_color_viridis(discrete = TRUE) +
+  scale_fill_viridis(discrete = TRUE)
 
 datadist_diagnosis_present <- Data %>%
-  group_by(Location,Diagnosis_of_disease) %>% 
+  group_by(Location, Diagnosis_of_disease) %>% 
   summarise(count = n()) %>% 
-  mutate(Dis_percentage = round(count/sum(count)*100),4) %>%
+  mutate(Dis_percentage = round(count/sum(count)*100), 4) %>%
   filter(Diagnosis_of_disease == "Present") %>%
   select(Dis_percentage)
 
@@ -66,12 +70,16 @@ diagnosis_present_string <- format_tsv(
   eol = "\n")
 
 # Make annotated plots
-Location_cholesterol_plot <- Location_cholesterol_plot + annotate("text", x = 550, y = 3, label = mean_string)
-Location_disease_plot <- Location_disease_plot + annotate("text", x = 150, y = 3, label = diagnosis_present_string)
-combined_plot <- ggarrange(Location_cholesterol_plot,Location_disease_plot)
+Location_cholesterol_plot <- Location_cholesterol_plot + 
+  annotate("text", x = 550, y = 3, label = mean_string)
+Location_disease_plot <- Location_disease_plot + 
+  annotate("text", x = 150, y = 3, label = diagnosis_present_string)
+combined_plot <- ggarrange(Location_cholesterol_plot, Location_disease_plot)
 combined_plot
 Location_plot <- annotate_figure(combined_plot,
                 top = ("Locations relation with cholesterol and heart disease"))
 
-ggsave(Location_plot, filename="/cloud/project/results/06_Location_plot.png", width = 16, height = 9, dpi = 72)
+ggsave(Location_plot, 
+       filename="/cloud/project/results/06_Location_plot.png", 
+       width = 16, height = 9, dpi = 72)
 
